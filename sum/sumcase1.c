a041a9bc282d9212cd375cdd130b156647f277c6
manikanta2k <bmanikanta296@gmail.com>
2023-04-12 13:47:41 +0000

    addition case1

---
+++ sum/sumcase1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+      //Declaration of the variables
+	  int a;
+	  int b;
+	  int result;
+	  
+	  //Assigning the values
+	  a = 10;
+	  b = 10;
+	  
+	  //Main logic
+	  result = a + b;
+	  
+	  //Printing the sum result
+	  printf("%d\n", result);
+
+	  //Exiting from the main logic
+	  return 0;
+}	  
+
+// 20
