a2bf4edbfac466674e0567be87543396cf6d0a3a
manikanta2k <bmanikanta296@gmail.com>
2023-04-12 15:02:14 +0000

    subtraction

---
+++ sub/subcase1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+          //Declaration of the variables
+	  int a;
+	  int b;
+	  int result;
+	  
+	  //Assigning the values
+	  a = 20;
+	  b = 10;
+	  
+	  //Main logic
+	  result = a - b;
+	  
+	  //Printing the sub result
+	  printf("%d\n", result);
+
+	  //Exiting from the main logic
+	  return 0;
+}	 
+
+
+
+// 10
---
+++ sub/subcase2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+        //Declaration of the variables
+	int a;
+	int b;
+	int result;
+	
+	//Main sub logic
+        result = a - b;
+	
+	//printing the sub output
+	printf("%d\n", result);
+	
+	//Exit from the main logic
+	return 0;
+
+}
+
+
+
+//garbage value 32767
