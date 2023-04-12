a248c79f4e416b608599aea57add3511d796d422
manikanta2k <bmanikanta296@gmail.com>
2023-04-12 15:16:14 +0000

    multiplication

---
+++ multi/multicase2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+        //Declaration of the variables
+	int a;
+	int b;
+	int result;
+	
+	//Main multiplication logic
+	result = a * b;
+	
+	//printing the output
+	printf("%d\n", result);
+	
+	//Exit from the main logic
+	return 0;
+
+}
+
+
+// garbage value 0
