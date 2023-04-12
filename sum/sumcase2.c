12d5448756111a8056e48706d1c1170dc2706624
manikanta2k <bmanikanta296@gmail.com>
2023-04-12 14:33:30 +0000

    mani

---
+++ sum/sumcase2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+        //Declaration of the variables
+	
+	int a;
+	int b;
+	int result;
+	
+	//Main sum logic
+	
+	result = a + b;
+	
+	//printing the sum output
+	
+	printf("%d\n", result);
+
+	//Exit from the main    logic
+	
+	return 0;
+
+
+}
+
+
+
+// garbage value 32765
---
+++ sum/sumcase3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+         //Declaration of the variables
+	 int a;
+	 int b;
+	 int result;
+	 
+	 //Main sum logic
+	 result = a + b;
+	 
+	 //Assigning the values
+	 a = 10;
+	 b = 10;
+	 
+	 //printing the sum output
+	 printf("%d\n", result);
+	 
+	 //Exiting from the main logic
+	 return 0;
+	 
+}
+
+
+
+// garbage value 32764
