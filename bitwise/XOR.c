c786aa243d1e391b58b95bbf8fb7fd528da3aaa6
manikanta2k <bmanikanta296@gmail.com>
2023-04-20 12:53:19 +0000

    bitwise XOR

---
+++ bitwise/XOR.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+
+       //Declaring the variables
+
+       int x;
+    
+       int o;
+   
+       int r;
+
+       //Assigning the values
+
+       x = 10;
+   
+       o = 20;
+    
+       //Applying the main logic
+
+       r = x^=o^=x^=o;
+
+       //Printing r value
+       
+       printf("%d\n", r);
+       printf("\n");
+
+       //Now printing x , o values
+       
+       printf("%d %d\n", x , o);
+
+       return 0;
+}       
+
+
+
+
+
+/* output
+ * 20
+
+20 10
+*/
+
+
+
