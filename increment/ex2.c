cecb8f3e4b985de12f475552e5d8361673ba4d5a
manikanta2k <bmanikanta296@gmail.com>
2023-04-22 18:19:42 +0000

    increment

---
+++ increment/ex2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    //Declaring the variables
+     int a;
+
+     int b;
+
+    //Assigning the value
+
+      a = 10;
+
+    //Applying the main logic
+
+      b = a++ + a++;
+
+    //Print the value of b
+
+      printf("%d %d\n",a, b);
+
+      return 0;
+
+}
+
+
+/* output 
+12 21
+*/
---
+++ increment/ex3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    //Declaring the variables
+
+     int a;
+
+     int b;
+
+    //Assigning the values
+ 
+      a = 10;
+
+    //Applying the main logic
+
+      b = ++a  + ++a ;
+
+    //printing the value of b
+
+      printf("%d %d\n",a, b);
+
+      return 0;
+
+}
+
+
+
+
+// output 12 24
+
